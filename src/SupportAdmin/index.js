e76ea0be8161ea1572b1b2c7eb53a4bc7fecc2c0
harish <harinani309@gmail.com>
2023-09-27 10:20:45 +0530

    chat-engine

---
+++ src/SupportAdmin/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { ChatEngine } from 'react-chat-engine'
+
+const SupportAdmin = () => {
+  return (
+    <ChatEngine 
+      projectID="0492351d-bd5d-4ff1-aaa5-28a8219984d1"
+      userName='bharath'
+      userSecret='12345'
+      height='calc(100vh - 12px)'
+    />
+  );
+}
+
+export default SupportAdmin;
